332574b2055eaac30cbbd058c550a618338a2f95
Geoffrey-Sciascia <gwsciascia@gmail.com>
2022-05-20 11:54:08 +1200

    use operators and default values

--- DuhPieShopHR/Program.cs
+++ DuhPieShopHR/Program.cs
@@ -6,7 +6,35 @@ namespace DuhPieShopHR
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int monthlyWage = 1234;
+            int months = 12, hours = 1000;
+
+            bool isActive = true;
+            double rating = 99.25;
+
+            double hourlyRate = 21.20;
+            int numberOfHoursWorked = 160;
+
+            double currentWage = hourlyRate * numberOfHoursWorked;
+
+            Console.WriteLine(currentWage);
+
+            hourlyRate += 3;
+
+            if(currentWage > 4000)
+            {
+                Console.WriteLine("Top gold employee");
+            }
+
+            int numberOfEmployees = 15;
+
+            numberOfEmployees--;
+
+            bool a;
+            int b;
+
+
+            Console.ReadLine();
         }
     }
 }
